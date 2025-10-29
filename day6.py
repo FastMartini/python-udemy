56809cab8b8abde288ec05b2d492bfc4b2430528
Diego Martinez <diegommart2004@gmail.com>
2025-10-29 15:42:06 -0400

    Learned about defining and calling functions; completed a challenge in reeborg.ca

---
+++ day6.py
@@ -0,0 +1,44 @@
+# defining and calling functions
+# Define the funtion:
+
+# def my_function():
+#     print("hello")
+#     print("bye")
+
+# Call the function:
+
+# my_function()
+#-------------------------------------
+#While Loops
+
+#while something_is_true:
+#   Do something repeatedly
+#-------------------------------------
+# Challenge in link below
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds/menus/reeborg_intro_en.json&name=Maze&url=worlds/tutorial_en/maze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def traverse_right():
+#     while wall_on_right():
+#         if wall_in_front():
+#             turn_left()
+#         else:
+#             move()
+#
+#     if not at_goal():
+#         turn_right()
+#         move()
+#
+#
+# while not at_goal():
+#     traverse_right()
+
+
+
+
+
